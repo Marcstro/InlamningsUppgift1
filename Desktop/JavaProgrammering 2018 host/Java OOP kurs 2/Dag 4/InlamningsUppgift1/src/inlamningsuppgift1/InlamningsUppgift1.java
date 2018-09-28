06ea50e446ebed875043aec67e0a77d56ea0aba9
Marcus Strom <marcus.strom.90@gmail.com>
2018-09-28 10:40:29 +0200

    6th version

--- Desktop/JavaProgrammering 2018 host/Java OOP kurs 2/Dag 4/InlamningsUppgift1/src/inlamningsuppgift1/InlamningsUppgift1.java
+++ Desktop/JavaProgrammering 2018 host/Java OOP kurs 2/Dag 4/InlamningsUppgift1/src/inlamningsuppgift1/InlamningsUppgift1.java
@@ -16,7 +16,7 @@ public class InlamningsUppgift1 {
         
         System.out.println("Hello world.");
             
-        System.out.println("This is version 4");
+        System.out.println("This is version 6");
     }
 
 }
