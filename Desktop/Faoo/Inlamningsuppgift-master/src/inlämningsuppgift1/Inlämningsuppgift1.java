fc242aa965f2c334cfcb927a97f67134b2a39817
Marcus Strom <marcus.strom.90@gmail.com>
2018-09-28 11:32:16 +0200

    4th av marcus

--- Desktop/Faoo/Inlamningsuppgift-master/src/inlämningsuppgift1/Inlämningsuppgift1.java
+++ Desktop/Faoo/Inlamningsuppgift-master/src/inlämningsuppgift1/Inlämningsuppgift1.java
@@ -2,16 +2,16 @@
  * Java Utveckling 18
  */
 
-package inl‰mningsuppgift1;
+package inl√§mningsuppgift1;
 
 
-public class Inl‰mningsuppgift1 {
+public class Inl√§mningsuppgift1 {
 
    
     public static void main(String[] args) {
         
-        System.out.println("jag ‰r kod nr 3");
-        System.out.println("version 4 av marcus");
+        System.out.println("jag √§r kod nr 3");
+        System.out.println("version 4 av marcus.");
     }
 
 }
