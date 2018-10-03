708829748401986594327a1adfe113e7886e3eac
Marcus Strom <marcus.strom.90@gmail.com>
2018-10-03 11:25:01 +0200

    Testing inheritance and interface implementation

---
+++ Desktop/JavaProgrammering 2018 host/Java OOP kurs 2/RandomProgram/TestInferface/src/Interf2.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+{licensePrefix} MARCUS DU AER AWESOME!!!
+ */
+
+
+public interface Interf2 {
+
+    public  String testaI2();
+    
+    default String InterMetod(){
+        return "Interface2 gav detta";
+    }
+    
+    public String testaI22();
+
+}
---
+++ Desktop/JavaProgrammering 2018 host/Java OOP kurs 2/RandomProgram/TestInferface/src/Test.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+{licensePrefix} MARCUS DU AER AWESOME!!!
+ */
+
+
+public class Test {
+
+    public static void main(String args[]){
+        
+        Mor m = new Mor();
+        Mor dm = new Dotter();
+        Dotter d = new Dotter();
+        m.testa();
+        dm.testa();
+        d.testa();
+        
+        System.out.println(m.testa2());
+        System.out.println(dm.testa2());
+        System.out.println(d.testa2());
+        
+        System.out.println(d.geResultat100());
+        
+        System.out.println(m.ord);
+        System.out.println(dm.ord);
+        System.out.println(d.ord);
+        
+        System.out.println(m.geord());
+        System.out.println(dm.geord());
+        System.out.println(d.geord());
+        
+//        System.out.println(m.testaI2());
+//        System.out.println(dm.testaI2());
+        System.out.println(d.testaI2());
+        
+        System.out.println(dm.testaI1());
+        System.out.println(m.testaI1());
+        System.out.println(d.testaI1());
+        
+        System.out.println(d.InterMetod());
+        
+        //System.out.println(m.testaI22);
+        dm.test103();
+        d.test103();
+        
+        
+        
+    }
+
+}
